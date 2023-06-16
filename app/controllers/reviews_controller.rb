9fa19b2888e97a6c453ff7c2a1bb69e85936057d
Sejal <sejaljain2803@gmail.com>
2023-06-16 18:26:51 +0530

    Integrate review functionality

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def new
+    @movie = Movie.find_by(imdb_id: params[:movie_id])
+    @review = Review.new  
+  end
+
+  def create
+    @movie = Movie.find_by(imdb_id: params[:movie_id])
+    @review = @movie.reviews.build(review_params)
+
+    if @review.save
+      redirect_to movie_path(params[:movie_id]), notice: "Review was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :movies
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end
   root "movies#index"
 end
